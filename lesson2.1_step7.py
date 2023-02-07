1e4f4e72537efac554c56129e95479fd3eb1ae7a
Валерия Любинская <valerialubinskaa@MacBook-Air-Valeria.local>
2023-02-07 15:05:48 +0400

    homework from stepik automatisation course lesson 2.1 step 7

---
+++ lesson2.1_step7.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "#treasure")
+    x_element = x_element.get_attribute("valuex")
+
+    x = x_element
+    print(x)
+    print(type(x))
+    y = calc(x)
+    input1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    input1.send_keys(y)
+
+    option1 = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    option1.click()
+
+    option1 = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    option1.click()
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(10)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+except Exception as e:
+    raise e
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(2)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
